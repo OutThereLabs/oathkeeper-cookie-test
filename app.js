769096efe64a437cac43ba2693e76494d4ef31e0
Patrick Tescher <pat2man@gmail.com>
2019-04-03 11:40:30 -0700

    Fix httpprovider

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const providerURL = new URL("/v1/web3/mainnet", window.location.href);
-const provider = new Web3.providers.HttpProvider(providerURL);
+const provider = new Web3.providers.HttpProvider(providerURL.href);
 const web3 = new Web3(provider);
 
 document.getElementById("cookie").innerHTML = document.cookie;
 
 web3.eth.getAccounts((error, accounts) => {
-    document.getElementById("accounts").innerHTML = JSON.stringify(accounts);
+    document.getElementById("accounts").innerHTML = JSON.stringify(error || accounts);
 })
