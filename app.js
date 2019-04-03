7c5c883f325f0b95e67adc5382dfea2e80d86b0c
Patrick Tescher <pat2man@gmail.com>
2019-04-03 11:21:56 -0700

    Test Web3

---
+++ app.js
@@ -0,0 +1,9 @@
+const providerURL = new URL("/v1/web3/mainnet", window.location.href);
+const provider = new Web3.providers.HttpProvider(providerURL);
+const web3 = new Web3(provider);
+
+document.getElementById("cookie").innerHTML = document.cookie;
+
+web3.eth.getAccounts((error, accounts) => {
+    document.getElementById("accounts").innerHTML = JSON.stringify(accounts);
+})
